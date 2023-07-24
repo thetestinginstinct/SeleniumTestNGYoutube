db6f76634c6fcf2176fde6b6e7fefc9a88bb0040
thetestinginstinct <thetestinginstinct@gmail.com>
2023-07-25 00:58:40 +0530

    run specific test data from DataProvider

---
+++ src/test/java/com/testng/dataproviders/UseSpecificSetOfDataFromDataProviderTest.java
@@ -0,0 +1,31 @@
+package com.testng.dataproviders;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class UseSpecificSetOfDataFromDataProviderTest {
+
+	@Test(dataProvider = "userData1")
+	public void createUser(String firstname, String lastname, String username, int mobile) {
+		System.out.println("Firstname is:" + firstname);
+		System.out.println("Lastname is:" + lastname);
+		System.out.println("Username is:" + username);
+		System.out.println("Mobile is:" + mobile);
+		System.out.println("\n");
+	}
+
+		@DataProvider(name = "userData1",indices = {4})
+	public Object[][] provideUserData() {
+		
+		Object[][] data1 = { 
+				{ "Tom", "Bob", "Tomy", 1234567890 }, 
+				{ "Tom1", "Bob1", "Tomy1", 1234567890 },
+				{ "Tom2", "Bob2", "Tomy2", 1234567890 },
+				{ "Tom3", "Bob3", "Tomy3", 1234567890 },
+				{ "Tom4", "Bob4", "Tomy4", 1234567890 }
+				};
+		
+		return data1;
+	}
+
+}
