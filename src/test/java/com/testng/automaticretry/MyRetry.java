1d6621402e00c093d2736391077efe7ac5b014de
thetestinginstinct <thetestinginstinct@gmail.com>
2023-09-10 22:40:34 +0530

    TestNG automatic retry

--- src/test/java/com/testng/automaticretry/MyRetry.java
+++ src/test/java/com/testng/automaticretry/MyRetry.java
@@ -1,12 +1,11 @@
 package com.testng.automaticretry;
 
 import org.testng.IRetryAnalyzer;
-import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class MyRetry implements IRetryAnalyzer,ITestListener{
+public class MyRetry implements IRetryAnalyzer{
 	private int retryCount = 0;
-	  private static final int maxRetryCount = 3;
+	  private static final int maxRetryCount = 2;
 	 
 	  @Override
 	  public boolean retry(ITestResult result) {
--- src/test/java/com/testng/automaticretry/TestclassSample.java
+++ src/test/java/com/testng/automaticretry/TestclassSample.java
@@ -4,9 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestclassSample {
-	
+
 	@Test(retryAnalyzer = MyRetry.class)
-	public void RetryTest() {
-	Assert.fail();
+	public void retryFailTest() {
+		Assert.fail();
+	}
+
+	@Test()
+	public void passTest() {
+		Assert.assertTrue(true);
 	}
 }
